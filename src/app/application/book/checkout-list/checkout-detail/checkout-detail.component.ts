8ad95b690ac8432084734de9387ffa062920b46c
unknown <quangcoltech@gmail.com>
2019-06-18 01:00:22 +0700

    update checkout-detail

--- src/app/application/book/checkout-list/checkout-detail/checkout-detail.component.ts
+++ src/app/application/book/checkout-list/checkout-detail/checkout-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-checkout-detail',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutDetailComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  name;
+  address;
+  orderInfo = [
+    {
+      name: 'Do Do',
+      address: 'Ha Dong, Ha Noi, Vietnam'
+    }
+  ];
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+  }
 
   ngOnInit() {
+    this.buildForm();
+    this.getOrderInfo();
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      payType: ['']
+    });
+    this.form.setValue({
+      payType: 'masterCard'
+    });
+  }
+
+  getOrderInfo() {
+    const orderInfoObj = this.orderInfo.find(it => it === it);
+    this.name = orderInfoObj.name;
+    this.address = orderInfoObj.address;
+  }
+
+  onSubmitData() {
+    const formValue = this.form.value;
+    const dataObject = {
+      payType: formValue.payType,
+      name: this.name,
+      address: this.address
+    };
+    console.log(dataObject);
   }
 
 }
